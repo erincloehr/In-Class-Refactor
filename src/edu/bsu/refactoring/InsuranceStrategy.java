13ab715aa0d71256326ca99121e3ca3eb09980eb
Erin Loehr <ecloehr@bsu.edu>
2019-04-04 12:28:57 -0400

    Extract Superclass

---
+++ src/edu/bsu/refactoring/InsuranceStrategy.java
@@ -0,0 +1,13 @@
+package edu.bsu.refactoring;
+
+public abstract class InsuranceStrategy {
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public abstract int getConstant();
+
+    public abstract double getWeight();
+
+    public abstract int getAdjustment();
+}
